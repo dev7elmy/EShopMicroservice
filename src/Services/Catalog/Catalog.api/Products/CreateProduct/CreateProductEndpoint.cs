8ade1dfff06c25d0979768f45208dd09f818caad
Helmy Mohamed <hmohamed@tachyhealth.com>
2024-09-12 17:41:05 +0300

    CQRS command and query user MediatR,Mapster and Carter

---
+++ src/Services/Catalog/Catalog.api/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Net;
+
+namespace Catalog.api.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/Products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProudectCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/Products/{response.Id}", response);
+            })
+              .WithName("CreateProduct")
+              .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+              .ProducesProblem(StatusCodes.Status400BadRequest)
+              .WithSummary("Create Proudect")
+              .WithDescription("Create Proudect");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.api.Models;
+using MediatR;
+
+namespace Catalog.api.Products.CreateProduct
+{
+    public record CreateProudectCommand(string Name,List<string> Category,string Description,string ImageFile, decimal Price)
+        :ICommand<CreateProudectResult>;
+    public record CreateProudectResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProudectCommand, CreateProudectResult>
+    {
+        public async Task<CreateProudectResult> Handle(CreateProudectCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price= command.Price
+            };
+            return new CreateProudectResult(Guid.NewGuid());
+            //throw new NotImplementedException();
+        }
+    }
+}
